bd2a755d72d853821f1fb6ebdef498aba2e89231
andrewheusser <andrew.heusser@gmail.com>
2017-10-05 15:53:09 -0400

    added describe function

--- hypertools/__init__.py
+++ hypertools/__init__.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 from .plot.plot import plot
+from .tools.load import load
 from .tools.analyze import analyze
 from .tools.reduce import reduce
 from .tools.align import align
 from .tools.normalize import normalize
+from .tools.describe import describe
